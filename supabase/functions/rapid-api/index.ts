7104b0d7399fcc6c9b45bda9398f7b580af7cf91
lucas <lucasdev2124@gmail.com>
2026-04-05 16:09:29 -0300

    Auto-commit after full sync (build simulation)

---
+++ supabase/functions/rapid-api/index.ts
@@ -0,0 +1,18 @@
+// Setup type definitions for built-in Supabase Runtime APIs
+import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
+interface reqPayload {
+  name: string
+}
+
+console.info('server started')
+
+Deno.serve(async (req: Request) => {
+  const { name }: reqPayload = await req.json()
+  const data = {
+    message: `Hello ${name} 2!`,
+  }
+
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json', Connection: 'keep-alive' },
+  })
+})
