e58a48f81e132596a8c4683c0e89553c69d1b995
lucas <lucasdev2124@gmail.com>
2026-04-05 21:18:13 -0300

    Auto-commit after full sync (build simulation)

--- supabase/functions/rapid-api/index.ts
+++ supabase/functions/rapid-api/index.ts
@@ -9,7 +9,7 @@ console.info('server started')
 Deno.serve(async (req: Request) => {
   const { name }: reqPayload = await req.json()
   const data = {
-    message: `Hello ${name} 2222232!`,
+    message: `Hello ${name} 22222322!?`,
   }
 
   return new Response(JSON.stringify(data), {
